e07c6613ba994ee31a5603bf96cfbf44bfa9b11b
Sharan Kumar <shrnk9@gmail.com>
2026-01-15 11:51:28 +0530

    code updated on arranging the Graph nodes and creating agent functions

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,40 @@
+from utils.model_loader import ModelLoader
+from prompt_library.prompt import SYSTEM_PROMPT
+from langgraph.graph import StateGraph, MessagesState, END, START
+from langgraph.prebuilt import ToolNode, tools_condition
+
+
+class GraphBuilder():
+    def __init__(self):
+        self.toold = [
+            # WeatherInfoTool(),
+            # PlaceSearchTool(),
+            # CalculatorTool(),
+            # CurrencyConverterTool()
+        ]
+
+
+        self.system_prompt = SYSTEM_PROMPT
+
+    def agent_function(self,state:MessagesState):
+        """Main agent function"""
+        user_question = state["messages"]
+        input_question = [self.system_prompt] + user_question
+        response = self.llm_with_tools.invoke(input_question)
+        return {"messages": [response]}
+
+    def build_graph(self):
+        graph_builder = StateGraph(MessagesState)
+        graph_builder.add_node("agent",self.agent_function)
+        graph_builder.add_node("tools", ToolNode(tools=self.tool))
+        graph_builder.add_edge(START,"agent")
+        graph_builder.add_conditional_edges("agent", tools_condition)
+        graph_builder.add_edge("tools","agent")
+        graph_builder.add_edge("agent",END)
+
+        self.graph = graph_builder.compile()
+        return self.graph
+
+
+    def __call__(self):
+        return self.build_graph()
